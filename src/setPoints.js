9eff7c31bd530b9cda467a302ebd1ac974456e12
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2021-06-17 15:27:43 +0530

    jhg

--- src/setPoints.js
+++ src/setPoints.js
@@ -1,6 +1,6 @@
 function setPoints(ID,value) {
     let BaseURL = 'http://ec2-52-15-138-114.us-east-2.compute.amazonaws.com:8080/';
-
+var l = 1;
     return new Promise((resolve, reject) =>{
         fetch(BaseURL+"setPoints/?Id=" + ID+"&points=" + value, {
             method: 'GET'
